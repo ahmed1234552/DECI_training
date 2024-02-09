8b848ec79eb8c5c59fdd3d3201fa528546476231
Ahmed Fathy <74154389+ahmed1234552@users.noreply.github.com>
2024-02-09 08:40:37 +0200

    add l2

---
+++ L2_1st month/challenge1.py
@@ -0,0 +1,15 @@
+#Challenge 1: “1000“ pts
+#Desired output: The code compares correctly between the values of “a” and “b”
+
+###
+a =+ 33
+b = 33
+if b < a:
+  print("b is greater than a")
+elif b < a:
+  print("b is less than a")
+elif a == b. 
+  print("a and b are equal")
+else:
+  print ("ABCD")
+###
